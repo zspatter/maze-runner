6e892b609e57404a83991f6bcb5a8d2b0b2c39b0
Zachary Patterson <zach.s.patterson@gmail.com>
2019-03-24 16:36:48 -0400

    Adds shortest path (Dijkstras) to solve the maze

--- maze-runner/maze_runner.pl
+++ maze-runner/maze_runner.pl
@@ -1,62 +1,73 @@
-% Student exercise profile
 :- set_prolog_flag(occurs_check, error).        % disallow cyclic terms
 :- set_prolog_stack(global, limit(8 000 000)).  % limit term space (8Mb)
 :- set_prolog_stack(local,  limit(2 000 000)).  % limit environment space
+:- dynamic(reversePath/2).
+ 
+% makes edges behave as undirected
+path(Source, Destination, Weight) :- pway(Destination, Source, Weight).
+path(Source, Destination, Weight) :- pway(Source, Destination, Weight).
+ 
+% if path is less than the currently stored path, replace it
+shorterPath([H|Path], Weight) :-		       
+	reversePath([H|T], W), !, Weight < W,          
+	retract(reversePath([H|_], _)),
+	format('~w is closer than ~w\n', [[H|Path], [H|T]]),
+	assert(reversePath([H|Path], Weight)).
 
-% Your program goes here
+% store new path
+shorterPath(Path, Weight) :-		       
+	format('New path:~w\n', [Path]),
+	assert(reversePath(Path, Weight)).
+ 
+% traverses each unvisited adjacent node then compares weight to current minimum weight
+% shortest path and weight are updated, then traverse to adjacent
+traverse(Source, Path, Weight) :-		    
+	path(Source, T, W),		    
+	not(memberchk(T, Path)),	    
+	shorterPath([T, Source|Path], Weight + W), 
+	traverse(T, [Source|Path], Weight + W).	    
+ 
+% removes solutions and traverses from original source
+traverse(Source) :-
+	retractall(reversePath(_,_)),           
+	traverse(Source, [], 0).              
+traverse(_).
+ 
+% finds all weights, once the destination is reached, path and distance are reported
+solve(Source, Destination) :-
+    format('Finding the shortest path between source node ~w and destination node ~w:\n\n', [Source, Destination]),
+	traverse(Source),                   
+	reversePath([Destination|ReversePath], Weight)->        
+	  reverse([Destination|ReversePath], Path),   
+	  CumulativeWeight is round(Weight),
+	  format('Shortest path is ~w with distance ~w = ~w\n\n',
+	       [Path, Weight, CumulativeWeight]);
+	format('There is no route from ~w to ~w\n', [Source, Destination]).
 
-pway(a, b, 3)
-pway(a, c, 1)
-pway(a, e, 4)
-pway(a, i, 2)
+% sample graph
+pway(a, b, 3).
+pway(a, c, 1).
+pway(a, e, 4).
+pway(a, i, 2).
+pway(b, d, 2).
+pway(b, e, 3).
+pway(b, g, 9).
+pway(b, h, 10).
+pway(b, i, 4).
+pway(c, d, 2).
+pway(c, f, 4).
+pway(c, g, 3).
+pway(d, f, 8).
+pway(d, j, 4).
+pway(e, h, 3).
+pway(e, i, 4).
+pway(f, i, 8).
+pway(f, j, 2).
+pway(g, h, 2).
+pway(g, j, 5).
+pway(h, i, 4).
 
-% pway(b, a, 3)
-pway(b, d, 2)
-pway(b, e, 3)
-pway(b, g, 9)
-pway(b, h, 10)
-pway(b, i, 4)
-
-% pway(c, a, 1)
-pway(c, d, 2)
-pway(c, f, 4)
-pway(c, g, 3)
-
-% pway(d, b, 2)
-% pway(d, c, 2)
-pway(d, f, 8)
-pway(d, j, 4)
-
-% pway(e, a, 4)
-% pway(e, b, 3)
-pway(e, h, 3)
-pway(e, i, 4)
-
-% pway(f, c, 4)
-% pway(f, d, 8)
-pway(f, i, 8)
-pway(f, j, 2)
-
-% pway(g, b, 9)
-% pway(g, c, 3)
-pway(g, h, 2)
-pway(g, j, 5)
-
-% pway(h, b, 10)
-% pway(h, e, 3)
-% pway(h, g, 2)
-pway(h, i, 4)
-
-% pway(i, a, 2)
-% pway(i, b, 4)
-% pway(i, e, 4)
-% pway(i, f, 8)
-% pway(i, h, 4)
-
-% pway(j, d, 4)
-% pway(j, f, 2)
-% pway(j, g, 5)
-
-/** <examples> Your example queries go here, e.g.
-?- member(X, [cat, mouse]).
-*/
+% sample queries
+?- solve(b, j).
+?- solve(a, c).
+ 
