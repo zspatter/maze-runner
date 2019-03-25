c42174ae368e4d7a1f302e012c3cf8f0613e6694
Zachary Patterson <localhost@zspatter.dev>
2019-03-25 13:59:24 -0400

    Changes implementation from shortest path to all paths
    Finds the shortest path, then incrementally increases length until no more paths are found. This script prevents cycles as well.

--- maze-runner/maze_runner.pl
+++ maze-runner/maze_runner.pl
@@ -1,46 +1,43 @@
 :- set_prolog_flag(occurs_check, error).        % disallow cyclic terms
 :- set_prolog_stack(global, limit(8 000 000)).  % limit term space (8Mb)
 :- set_prolog_stack(local,  limit(2 000 000)).  % limit environment space
-:- dynamic(reversePath/2).
- 
-% makes edges behave as undirected
-path(Source, Destination, Weight) :- pway(Source, Destination, Weight) ; pway(Source, Destination, Weight).
- 
-% if path is less than the currently stored path, replace it
-shorterPath([H|Path], Weight) :-		       
-	reversePath([H|_], W), !, Weight < W,          
-	retract(reversePath([H|_], _)),
-	assert(reversePath([H|Path], Weight)).
 
-% store new path
-shorterPath(Path, Weight) :-		       
-	assert(reversePath(Path, Weight)).
- 
-% traverses each unvisited adjacent node then compares weight to current minimum weight
-% shortest path and weight are updated, then traverse to adjacent
-traverse(Source, Path, Weight) :-		    
-	path(Source, T, W),		    
-	not(memberchk(T, Path)),	    
-	shorterPath([T, Source|Path], Weight + W), 
-	traverse(T, [Source|Path], Weight + W).	    
- 
-% removes solutions and traverses from original source
-traverse(Source) :-
-	retractall(reversePath(_,_)),           
-	traverse(Source, [], 0).              
-traverse(_).
- 
-% finds all weights, once the destination is reached, path and distance are reported
-solve(Source, Destination) :-
-    format('Finding the shortest path between ~w (source) and ~w (destination):\n', [Source, Destination]),
-	traverse(Source),                   
-	reversePath([Destination|ReversePath], Weight)->        
-	  reverse([Destination|ReversePath], Path),   
-	  CumulativeWeight is round(Weight),
-	  format('\tShortest path: P = ~w \n\tWeight: N = ~w = ~w\n\n',
-	       [Path, Weight, CumulativeWeight]);
-	format('\tThere are no paths connecting nodes ~w and ~w!\n', [Source, Destination]).
+% makes graph behave as undirected
+path(Source, Destination, Weight) :- pway(Source, Destination, Weight) ; pway(Destination, Source, Weight).
 
+% finds shortest paths incrementally. Arbitrary ceiling of 1,000 is set.
+solve(Source, Destination, Path, Weight) :-
+    verifyCurrentWeight(Source, Destination, 0, 1000, Path, Weight).
+    
+% finds paths with an overall weight equal to CurrentWeight
+verifyCurrentWeight(Source, Destination, CurrentWeight, _, Path, CummulativeWeight) :-
+	pfWrapper(Source, Destination, Path, RecursiveWeight),
+    CurrentWeight =:= RecursiveWeight,
+    CummulativeWeight is RecursiveWeight.
+
+% increments the CurrentWeight variable if there is no path with a length of CurrentWeight
+verifyCurrentWeight(Source, Destination, CurrentWeight, MaxWeight, Path, CummulativeWeight) :-
+    CurrentWeight < MaxWeight,
+    IncrementedWeight is CurrentWeight + 1,
+    verifyCurrentWeight(Source, Destination, IncrementedWeight, MaxWeight, Path, CummulativeWeight).
+
+pfWrapper(Source, Destination, Path, CummulativeWeight) :-
+    pathFinder(Source, Destination, [Source], Path, CummulativeWeight).
+
+% destination reached
+pathFinder(Previous, Destination, CurrentPath, Path, CummulativeWeight) :-
+    pway(Previous, Destination, CummulativeWeight),
+   % not(member(Previous, CurrentPath)),
+    append(CurrentPath, [Destination], Path).
+
+% recursive case
+pathFinder(Source, Destination, CurrentPath, Path, CummulativeWeight) :-
+    path(Source, Next, Weight),
+    not(member(Next, CurrentPath) ; member(Destination, CurrentPath)),
+    append(CurrentPath, [Next], NewPath),
+    pathFinder(Next, Destination, NewPath, Path, RecursiveWeight),
+    CummulativeWeight is Weight + RecursiveWeight.
+ 
 % sample graph
 pway(a, b, 3).
 pway(a, c, 1).
@@ -65,6 +62,5 @@ pway(g, j, 5).
 pway(h, i, 4).
 
 % sample queries
-?- solve(b, j).
-?- solve(a, c).
- 
+% ?- solve(b, j, Path, Weight).
+% ?- solve(a, c, Path, Weight).
