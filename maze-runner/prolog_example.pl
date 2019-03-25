5522f6352ff27342ffdaca6616b1e8a37d114ac1
Zachary Patterson <zach.s.patterson@gmail.com>
2019-03-24 20:44:49 -0400

    Adds example with unweighted graphs

---
+++ maze-runner/prolog_example.pl
@@ -0,0 +1,25 @@
+/* prolog tutorial 2.15 Graph structures and paths */
+
+edge(1,2).
+edge(1,4).
+edge(1,3).
+edge(2,3).
+edge(2,5).
+edge(3,4).
+edge(3,5).
+edge(4,5).
+
+connected(X,Y) :- edge(X,Y) ; edge(Y,X).
+
+path(A,B,Path) :-
+       travel(A,B,[A],Q), 
+       reverse(Q,Path).
+
+travel(A,B,P,[B|P]) :- 
+       connected(A,B).
+travel(A,B,Visited,Path) :-
+       connected(A,C),           
+       C \== B,
+       \+member(C,Visited),
+       travel(C,B,[C|Visited],Path).  
+
