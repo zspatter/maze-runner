97dcfa9776a4482b5601a3833c4073d4f3e2f6d6
Zachary Patterson <localhost@zspatter.dev>
2019-03-26 16:38:54 -0400

    Adds sample mazes

--- maze-runner/sample_mazes.pl
+++ maze-runner/sample_mazes.pl
@@ -1,4 +1,106 @@
-set_eol Factory_Cal UNLOCK 
-set_eol Tom_Tom_Map_Region 4
-set_eol Factory_Cal LOCK 
+% sample graph #0 (present in maze_runner script)
+pway(a, b, 3).
+pway(a, c, 1).
+pway(a, e, 4).
+pway(a, i, 2).
+pway(b, d, 2).
+pway(b, e, 3).
+pway(b, g, 9).
+pway(b, h, 10).
+pway(b, i, 4).
+pway(c, d, 2).
+pway(c, f, 4).
+pway(c, g, 3).
+pway(d, f, 8).
+pway(d, j, 4).
+pway(e, h, 3).
+pway(e, i, 4).
+pway(f, i, 8).
+pway(f, j, 2).
+pway(g, h, 2).
+pway(g, j, 5).
+pway(h, i, 4).
 
+
+% sample graph #1
+pway(a, b, 4).
+pway(a, h, 8).
+pway(b, h, 11).
+pway(b, c, 8).
+pway(h, i, 7).
+pway(h, g, 1).
+pway(c, i, 2).
+pway(i, g, 6).
+pway(g, f, 2).
+pway(c, f, 4).
+pway(c, d, 7).
+pway(d, e, 9).
+pway(d, e, 14).
+pway(f, e, 10).
+
+
+% sample graph #2
+pway(a, b, 2).
+pway(b, c, 2).
+pway(c, d, 1).
+pway(c, e, 3).
+pway(e, f, 2).
+pway(f, g, 1).
+pway(g, h, 6).
+pway(g, i, 6).
+pway(i, j, 3).
+pway(j, k, 3).
+pway(k, l, 4).
+pway(l, m, 1).
+pway(m, n, 8).
+pway(i, o, 2).
+pway(o, p, 4).
+pway(p, q, 2).
+pway(q, r, 4).
+pway(q, s, 1).
+pway(s, t, 2).
+
+
+% sample graph #3
+pway(a, b, 5).
+pway(a, c, 3).
+pway(a, d, 25).
+pway(b, e, 8).
+pway(b, f, 12).
+pway(e, h, 5).
+pway(e, i, 7).
+pway(c, g, 15).
+pway(g, j, 6).
+
+
+% sample graph #4
+pway(a, b, 3).
+pway(a, c, 5).
+pway(a, g, 8).
+pway(a, f, 1).
+pway(f, d, 14).
+pway(f, e, 6).
+pway(a, g, 7).
+pway(e, g, 9)
+pway(h, i, 2).
+pway(j, k, 4).
+pway(j, l, 8).
+pway(j, m, 14).
+pway(l, m, 2).
+
+
+% sample graph #5
+pway(a, b, 5).
+pway(a, c, 6).
+pway(b, c, 7).
+pway(b, d, 3).
+pway(b, e, 2).
+pway(c, e, 7).
+pway(c, g, 8).
+pway(c, h, 4).
+pway(d, e, 1).
+pway(e, f, 9).
+pway(g, h, 7).
+pway(i, j, 15).
+pway(k, l, 12).
+pway(l, m, 2).
